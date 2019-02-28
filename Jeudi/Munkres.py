7055215b92cfe264571d835fa0cd01a6a7cec006
Pierre OREISTEIN <polostan@live.fr>
2019-02-28 19:49:39 +0100

    Change

--- Jeudi/Munkres.py
+++ Jeudi/Munkres.py
@@ -1,18 +1,20 @@
-from Munkres.munkres import Munkres
+from Munkres.munkres import Munkres, print_matrix
 
-m = Munkres()
-
-from munkres import Munkres, print_matrix
-
+# Matrix of costs
 matrix = [[5, 9, 1],
           [10, 3, 2],
           [8, 7, 4]]
+
+# Instantiate a Munkres object
 m = Munkres()
 indexes = m.compute(matrix)
+
 print_matrix(matrix, msg='Lowest cost through this matrix:')
+
 total = 0
 for row, column in indexes:
     value = matrix[row][column]
     total += value
     print(f'({row}, {column}) -> {value}')
+
 print(f'total cost: {total}')'
