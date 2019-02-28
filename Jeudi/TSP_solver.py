12faccc291cf6d8460e75f251e6f08c9651fa536
AF018 <a.francois962@laposte.net>
2019-02-28 21:50:19 +0100

    For Pierre 2

--- Jeudi/TSP_solver.py
+++ Jeudi/TSP_solver.py
@@ -4,11 +4,10 @@ from time import time
 from tqdm import tqdm
 from numba import jit
 
-@jit(nopython=True)
 def compute_scores(word_sets):
     word_nb = len(word_sets)
     score_array = np.zeros((word_nb, word_nb))
-    for i in range(word_nb):
+    for i in tqdm(range(word_nb)):
         set_i = word_sets[i]
         size_i = len(set_i)
         for j in range(i+1, word_nb):
@@ -19,25 +18,48 @@ def compute_scores(word_sets):
             score_array[j,i] = score_array[i,j]
     return score_array
 
+
+def load_vertical(data_path, data_name):
+    or_data_v = np.load("Save\\assignment_dct_100.npy").item()
+    or_data_v = or_data_v[data_name]
+
+    v_ids = or_data_v['assignment']
+    v_tags = or_data_v['tags_save']
+    v_tags = [set(tag_list) for tag_list in v_tags]
+
+    return v_tags, v_ids
+
 def load(data_path):
     # Load data
     or_data = np.load("Save\\data_dct.npy").item()
+    data_name = 'a_examplet'
+
     # print(or_data.keys())
     # print(machin['a_examplet'].iloc[0, :])
     # Get ID Horizontal + IDs vertical pairs
     #for i in range()
-    or_data = or_data['b_lovely_landscapest']
+    or_data = or_data[data_name] 
     or_data_h = or_data[or_data['H_V'] == 'H']
-    id_h = list(or_data_h.iloc[:,0])[:1000]
-    
+    id_h = list(or_data_h.iloc[:,0])
+
     # Get sets of tags
     word_strings = or_data_h.iloc[:,4]
-    word_sets = [set([int(str_idx) for str_idx in s.split(" ")]) for s in word_strings][:1000]
+    word_sets = [set([int(str_idx) for str_idx in s.split(" ")]) for s in word_strings]
+    
+    v_tags, v_ids = load_vertical(data_path, data_name)
+
+    print(v_tags, v_ids)
+
+    ids = id_h + v_ids
+    word_sets = word_sets + v_tags
+
+    print(word_sets)
+    
     # Compute array
     score_array = compute_scores(word_sets)
     # Save the array
     np.save("tsp_score_array.npy", score_array)
-    return score_array, id_h
+    return score_array, ids
 
 def greedy_solver(score_array):
     vertex_idx = 0
@@ -68,11 +90,11 @@ def make_output(greedy_solution, df_name, ids):
 start_t = time()
 
 score_array, ids = load('r')
-print(score_array[2])
+print(score_array)
 sol = TSP_solver(score_array)
 
 dico_sol = make_output(sol, "b_lovely_landscapest", ids)
-#print(dico_sol)
+print(dico_sol)
 
 end_t = time()
 print(end_t - start_t)
